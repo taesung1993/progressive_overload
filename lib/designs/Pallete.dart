3885d81b63c8bbcdb702a9e18bb10f3181f4b339
taesung1993 <cheonyulin@gmail.com>
2023-12-21 00:53:21 +0900

    create design global constants

---
+++ lib/designs/Pallete.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+enum Pallete {
+  black,
+  darkGrey,
+  grey,
+  lightGrey,
+  white,
+  primary1,
+  primary2,
+  primary3,
+  red1,
+  red2,
+  green1,
+  green2,
+  blue1,
+  blue2
+}
+
+const Map<Pallete, Color> pallete = {
+  Pallete.black: Color(0xff000000),
+  Pallete.darkGrey: Color(0xff9b9b9b),
+  Pallete.grey: Color(0xffd5d5d5),
+  Pallete.lightGrey: Color(0xfff2f2f2),
+  Pallete.white: Color(0xffffffff),
+  Pallete.primary1: Color(0xffff6000),
+  Pallete.primary2: Color(0xffffd3b9),
+  Pallete.primary3: Color(0xfffff3ec),
+  Pallete.red1: Color(0xffdb1307),
+  Pallete.red2: Color(0xffffb8b4),
+  Pallete.green1: Color(0xff34c759),
+  Pallete.green2: Color(0xff8ddc9f),
+  Pallete.blue1: Color(0xff007aff),
+  Pallete.blue2: Color(0xff93c6fc),
+};
---
+++ lib/designs/Typo.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+enum Typos {
+  H1_700,
+  H2_600,
+  H3_700,
+  H3_600,
+  H3_500,
+  T1_500,
+  T1_400,
+  T2_500,
+}
+
+const defaultFont = GoogleFonts.roboto;
+
+final Map<Typos, TextStyle> typos = {
+  Typos.H1_700: defaultFont(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    height: 1.08,
+  ),
+  Typos.H2_600: defaultFont(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    height: 1.3,
+  ),
+  Typos.H3_700: defaultFont(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    height: 1.5,
+  ),
+  Typos.H3_600: defaultFont(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    height: 1.5,
+  ),
+  Typos.H3_500: defaultFont(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    height: 1.5,
+  ),
+  Typos.T1_500: defaultFont(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 1.43,
+  ),
+  Typos.T1_400: defaultFont(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.43,
+  ),
+  Typos.T2_500: defaultFont(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+  ),
+};
