87158179bbfa2d2fd60c4f3a7f8d93acb5ab00c4
taesung1993 <cheonyulin@gmail.com>
2024-01-10 19:54:37 +0900

    change _openCreatingWorkoutBottomSheet with parameter and curry

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/rendering.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:progressive_overload/providers/date_provider.dart';
 import 'package:progressive_overload/screens/home_screen.dart';
 import 'package:progressive_overload/widgets/creating_workout_bottom_sheet.dart';
 import 'package:progressive_overload/widgets/gnb.dart';
@@ -12,37 +13,39 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
-  void _openCreatingWorkoutBottomSheet(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(32),
-          topRight: Radius.circular(32),
-        ),
-      ),
-      builder: (context) {
-        return const CreatingWorkoutBottomSheet();
-      },
-    );
+  _openCreatingWorkoutBottomSheet(DateTime now) {
+    return (BuildContext context) => showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(32),
+              topRight: Radius.circular(32),
+            ),
+          ),
+          builder: (context) {
+            return CreatingWorkoutBottomSheet(now: now);
+          },
+        );
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final _now = ref.watch(dateProvider);
+
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: HomeScreen(
-            createWorkout: _openCreatingWorkoutBottomSheet,
+            createWorkout: _openCreatingWorkoutBottomSheet(_now),
           ),
         ),
         bottomNavigationBar: GNB(
-          createWorkout: _openCreatingWorkoutBottomSheet,
+          createWorkout: _openCreatingWorkoutBottomSheet(_now),
         ),
       ),
     );
