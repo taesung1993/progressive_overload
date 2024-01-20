3faeab5e42fae7fd27e1dae00f9f3275adcdd61c
taesung1993 <cheonyulin@gmail.com>
2024-01-21 01:14:49 +0900

    change initial value with only date value

--- lib/providers/date_provider.dart
+++ lib/providers/date_provider.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DateNotifier extends StateNotifier<DateTime> {
-  DateNotifier() : super(DateTime.now());
+  DateNotifier() : super(DateUtils.dateOnly(DateTime.now()));
 
   void changeDate(DateTime time) {
     state = time;
