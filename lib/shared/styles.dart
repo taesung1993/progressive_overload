056aafdf53738b61205520576d37ab86c2de0d9d
윤태성 <masonyun@yuntaeseongs-MacBook-Pro.local>
2024-10-18 01:03:56 +0900

    add design system

---
+++ lib/shared/styles.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const TextStyle head1Bold = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle head2Bold = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle head3Bold = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle text1Medium = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle text1Regular = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+);
+
+const TextStyle text2Medium = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
+
+const Color primary1Color = Color(0xffff6000);
+const Color primary2Color = Color(0xffffd3b9);
+const Color primary3Color = Color(0xfffff3ec);
+const Color red1 = Color(0xffdb1307);
+const Color red2 = Color(0xffffb8b4);
+const Color green1 = Color(0xff34c759);
+const Color green2 = Color(0xff8ddc93);
+const Color blue1 = Color(0xff007aff);
+const Color blue2 = Color(0xff93c6fc);
+const Color black = Color(0xff000000);
+const Color darkgrey = Color(0xff9b9b9b);
+const Color grey = Color(0xffd5d5d5);
+const Color lightgrey = Color(0xfff2f2f2);
+const Color white = Color(0xffffffff);
